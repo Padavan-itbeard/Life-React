50532e0990eb5d695b314111a0d5a449f0440b39
Aleksandr Ivanov <aleksand7@yandex.ru>
2020-03-31 11:18:52 +0300

    Updates

---
+++ precache-manifest.e79ca1be28652ab5137301270e5010b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8356aed55733e61e18d981de33f21de8",
+    "url": "/Life-React/index.html"
+  },
+  {
+    "revision": "c0f074798b6efc9b5191",
+    "url": "/Life-React/static/css/main.cc0d1c34.chunk.css"
+  },
+  {
+    "revision": "e14c3ad9d9e09bbac207",
+    "url": "/Life-React/static/js/2.dfdc88d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Life-React/static/js/2.dfdc88d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0f074798b6efc9b5191",
+    "url": "/Life-React/static/js/main.27608a5e.chunk.js"
+  },
+  {
+    "revision": "f8356b902754a5b054a5",
+    "url": "/Life-React/static/js/runtime-main.ae2a427d.js"
+  }
+]);
